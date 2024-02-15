16d0534a9ca7c5c56a961f973e4309c367d8ca36
Atila Rampazo <atilarampazo@gmail.com>
2024-02-15 08:33:30 -0300

    HEY-13: Create like question

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,4 +1,26 @@
 @props(['question'])
-<div class="rounded dark:bg-gray-800/50 bg-white shadow shadow-blue-500/50 m-3 p-3 dark:text-gray-400 text-back">
-    {{$question->question}}
+<div class="
+rounded dark:bg-gray-800/50 bg-white shadow shadow-blue-500/50 m-3 p-3 dark:text-gray-400 text-back
+flex justify-between items-center
+">
+    <span>{{$question->question}}</span>
+    <div class="flex flex-row gap-3">
+        <x-form :action="route('question.like',$question)">
+            <button
+                    title="Click to like this question"
+                    class="flex items-center gap-1  text-green-500 hover:text-green-300 cursor-pointer "
+                    type="submit"
+            >
+                <x-icons.thumbs-up class="w-5 h-5" id="thumbs-up"/>
+                {{$question->likes  ?? 0}}
+            </button>
+        </x-form>
+        <x-form :action="route('question.like',$question)">
+            <button title="Click to like this question"
+                    class="text-red-700 hover:text-red-900 cursor-pointer flex items-center gap-1">
+                <x-icons.thumbs-down class="w-5 h-5" id="thumbs-up"/>
+                {{$question->unlikes  ?? 0}}
+            </button>
+        </x-form>
+    </div>
 </div>
