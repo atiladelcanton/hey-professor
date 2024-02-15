d6fa47daeaff31a105593ccd98dfcb6bd11012dd
Atila Rampazo <atilarampazo@gmail.com>
2024-02-15 09:08:49 -0300

    HEY-13: handling N+1 issue

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -4,6 +4,7 @@ rounded dark:bg-gray-800/50 bg-white shadow shadow-blue-500/50 m-3 p-3 dark:text
 flex justify-between items-center
 ">
     <span>{{$question->question}}</span>
+
     <div class="flex flex-row gap-3">
         <x-form :action="route('question.like',$question)">
             <button
@@ -12,14 +13,14 @@ flex justify-between items-center
                     type="submit"
             >
                 <x-icons.thumbs-up class="w-5 h-5" id="thumbs-up"/>
-                {{$question->likes  ?? 0}}
+                {{$question->votes_sum_like  ?? 0}}
             </button>
         </x-form>
         <x-form :action="route('question.unlike',$question)">
             <button title="Click to like this question"
                     class="text-red-700 hover:text-red-900 cursor-pointer flex items-center gap-1">
                 <x-icons.thumbs-down class="w-5 h-5" id="thumbs-up"/>
-                {{$question->unlikes  ?? 0}}
+                {{$question->votes_sum_unlike  ?? 0}}
             </button>
         </x-form>
     </div>
