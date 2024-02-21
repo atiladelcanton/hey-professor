9a567f16f22582032a9947c8c5c091332154ce61
Atila Rampazo <atilarampazo@gmail.com>
2024-02-21 08:38:36 -0300

    HEY-99: Ajuste

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -28,16 +28,17 @@
                     <x-table.tr>
                         <x-table.td>{{$question->question}}</x-table.td>
                         <x-table.td>
-                            <x-form :action="route('question.publish',$question)" put>
-                                <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 " >
-                                    Publicar
-                                </button>
-                            </x-form>
                             <x-form :action="route('question.destroy',$question)" put>
                                 <button type="submit">
                                     Deletar
                                 </button>
                             </x-form>
+                            <x-form :action="route('question.publish',$question)" put>
+                                <button type="submit" >
+                                    Publicar
+                                </button>
+                            </x-form>
+                            <a href="{{route('question.edit',$question)}}">Editar</a>
                         </x-table.td>
                     </x-table.tr>
                 @endforeach
