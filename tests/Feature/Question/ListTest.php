0e1e549ab41502e8f456c768abf97cd3bf9dc33d
Atila Rampazo <atilarampazo@gmail.com>
2024-02-22 08:38:35 -0300

    HEY-21: it should order by like and unlike, most like question should be a the top, most unlike questions should be in the button

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -26,3 +26,27 @@ it('should paginate the result', function () {
     get(route('dashboard'))
         ->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
 });
+
+it('should order by like and unlike, most like question  should  be a the top, most unlike questions should be in the button', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion = Question::find(3);
+    $user->like($mostLikedQuestion);
+
+    $mostUnlikedQuestion = Question::find(1);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
+        });
+});
